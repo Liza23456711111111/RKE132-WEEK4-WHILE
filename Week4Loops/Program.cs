8d296f7e15f1764cb8e7b680f2643f93f51b1d77
Skorobogatova Jelizaveta <jelizaveta.skorobogatova@tktk.ee>
2023-05-21 14:32:59 +0300

    Week4Loops

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//For-Loop
+//Dry-principle - Do not Repeat Yourself
+
+int i = 0;
+
+for (i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
